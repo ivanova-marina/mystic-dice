08b85e47b5cc31df2ae639f8f81209e0bcebf9b3
Marina Ivanova <marina0757@gmail.com>
2025-03-17 23:01:45 +0900

    test three

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
-    <div>
-      <div className="text-red-500"> Test </div>
+    <div id="canvas-container">
+      <Canvas>
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshPhongMaterial />
+        </mesh>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+      </Canvas>
     </div>
   );
 }
